8537e869d97f1b43e3ca0830ccd8c50819b29116
Glitch (magical-paint-hubcap) <none>
2020-08-12 10:38:25 +0000

    🎉🚗 Checkpoint ./server.js:17702624/1131

--- server.js
+++ server.js
@@ -4,29 +4,28 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const {graphqlHTTP} = require('express-graphql')
+const { buildSchema } = require('graphql')
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
